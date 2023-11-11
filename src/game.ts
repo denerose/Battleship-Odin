f6b655f7e035ab2d72b767dea1d2643bfcc77e9a
“Saphron” <109723086+denerose@users.noreply.github.com>
2023-11-11 13:09:32 +1100

    Place ship function v1

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,20 @@
+import { Ship } from "./ship"
 
-interface Tile {
-    x: number
-    y: number
-    occupied: boolean
+class Tile {
+
+    constructor(
+        public x: number,
+        public y: number,
+        myBoard?: GameBoard,
+        public occupied: boolean = false,
+        public hit: boolean = false,
+        public shipKey?: Ship) { }
+
+    triggerPlaceShip() {
+        if (!this.occupied) {
+
+        } else { return new Error('occupied!!') }
+    }
 }
 
 export class GameBoard {
@@ -13,8 +25,39 @@ export class GameBoard {
     ) {
         for (let x = 0; x < size; x++) {
             for (let y = 0; y < size; y++) {
-                this.gameBoard.push({ x, y, occupied: false })
+                this.gameBoard.push(new Tile(x, y))
             }
         }
     }
+
+    checkTiles(startTile: Tile, length: number) {
+        const tilesToCheck = []
+        let currentTile: Tile = startTile
+        if (tilesToCheck.length === 0) {
+            tilesToCheck.push(currentTile)
+        }
+        while (length > tilesToCheck.length) {
+            let newTile = this.gameBoard.find((newTile) => newTile.x === currentTile.x && newTile.y === currentTile.y + 1);
+            if (newTile) {
+                currentTile = newTile
+                tilesToCheck.push(currentTile)
+            } else return []
+        }
+        return tilesToCheck;
+    }
+
+    public placeShip(type: string, size: number, startTile: Tile) {
+        const placementArea = this.checkTiles(startTile, size)
+        if (placementArea.length === 0) { return new Error('Ship will overflow the board') }
+        else if (placementArea.some((tile) => tile.occupied)) { return new Error('Already occupied') }
+        else {
+            const shipToPlace = new Ship(type, size)
+            placementArea.forEach((tile) => {
+                tile.occupied = true
+                tile.shipKey = shipToPlace
+            })
+        }
+    }
+
+
 }
